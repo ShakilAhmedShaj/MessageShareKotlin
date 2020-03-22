52e71afbc756cd3528501b7b03ec362cb3363eb7
SHAZ <shakilahmedshaj@gmail.com>
2020-03-22 23:43:46 +0600

    Basic toast and Click

--- app/src/main/java/com/shaj/t3ch/messagesharekotlin/MainActivity.kt
+++ app/src/main/java/com/shaj/t3ch/messagesharekotlin/MainActivity.kt
@@ -2,11 +2,27 @@ package com.shaj.t3ch.messagesharekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+     btn_show_toast.setOnClickListener {
+
+         Log.i("MainActivity","Show Button Clicked!")
+         Toast.makeText(this,"Button Clicked",Toast.LENGTH_LONG).show()
+
+
+
+     }
+
+
+
     }
 }
